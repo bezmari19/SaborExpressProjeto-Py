54693af386014a6a9d8cd2cbc4c2b867fe5ab1a3
bezmari19 <marina.bezerraf19@gmail.com>
2025-05-07 18:49:29 -0300

    commit feito

--- app.py
+++ app.py
@@ -1,12 +1,39 @@
-print('Ｓａｂｏｒ Ｅｘｐｒｅｓｓ\n')
+import os
 
-print('1. Cadastrar restaurante')
-print('2. Listar restaurantes')
-print('3. Ativar restaurante')
-print('4. Sair\n')
+def exibir_nomePrograma():
+    print('Ｓａｂｏｒ Ｅｘｐｒｅｓｓ\n')
 
-opcao_escolhida = input('Escolha uma opção:')
-print(f'Você escolheu a opção: {opcao_escolhida}!\n')
+def exibir_opcoes():
+    print('1 - Cadastrar restaurante')
+    print('2 - Listar restaurantes')
+    print('3 - Ativar restaurante')
+    print('4 - Sair do app\n')
+
+def finalizar_app():
+    os.system('cls')
+    print('Saindo do app...\nObrigado por escolhar o Sabor Express!\n')
+
+def escolher_opcao():
+    opcao_escolhida = int(input('Escolha uma opção:'))
+    print(f'Você escolheu a opção: {opcao_escolhida}\n')
+
+    if opcao_escolhida == 1:
+        print('Cadastrar restaurante')
+    elif opcao_escolhida == 2:
+        print('Listar restaurantes')
+    elif opcao_escolhida == 3:
+        print('Ativar restaurante')
+    else:
+        finalizar_app()
+
+def main():
+    exibir_nomePrograma()
+    exibir_opcoes()
+    escolher_opcao()
+
+if __name__ == '__main__':
+    # Executa o código principal do aplicativo
+    main()
 
 
 
